a756ef627d35b3cab538fbe8aa1fd13583202807
chiragjain1401 <chiragjainhtml@gmail.com>
2023-04-12 18:44:19 +0530

    customercrud

--- warehouse.sql
+++ warehouse.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2023 at 01:42 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Apr 12, 2023 at 03:10 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -119,6 +119,36 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `units`
+--
+
+CREATE TABLE `units` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `area` varchar(255) NOT NULL,
+  `height` varchar(255) NOT NULL,
+  `width` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `warehouse_id` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `units`
+--
+
+INSERT INTO `units` (`id`, `name`, `area`, `height`, `width`, `location`, `warehouse_id`, `status`, `deleted_at`, `created_at`, `updated_at`) VALUES
+(1, 'suraj', '454', '100', '23', 'mumbai', '2', '0', NULL, '2023-04-12 07:11:19', '2023-04-12 07:11:19'),
+(2, 'suraj', '454', '100', '23', 'mumbai', '2', '0', NULL, '2023-04-12 07:12:26', '2023-04-12 07:12:26'),
+(3, 'suraj', '454', '100', '23', 'mumbai', '2', '0', NULL, '2023-04-12 07:12:56', '2023-04-12 07:12:56'),
+(4, 'weight loss', '454', '84', '49', 'Mumbai', '3', '1', NULL, '2023-04-12 07:22:16', '2023-04-12 07:22:16');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -129,6 +159,14 @@ CREATE TABLE `users` (
   `mobile` varchar(255) NOT NULL,
   `roles` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `gstin` varchar(255) NOT NULL,
+  `plan` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `start_date` varchar(255) NOT NULL,
+  `end_date` varchar(255) NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `remember_token` varchar(100) DEFAULT NULL,
@@ -141,8 +179,38 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `mobile`, `roles`, `status`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Admin', 'admin@warehouse.com', '7021640778', 'SuperAdmin', '1', NULL, '$2a$10$2ZWqz5BdJrqdD6KxxTIdtOW1VP5hR68iVTUQW9bjbiYxiUftf7jXG', NULL, NULL, NULL, NULL);
+INSERT INTO `users` (`id`, `name`, `email`, `mobile`, `roles`, `status`, `company_name`, `gstin`, `plan`, `address`, `state`, `city`, `start_date`, `end_date`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Admin', 'admin@warehouse.com', '7021640778', 'SuperAdmin', '1', '', '', '', '', '', '', '', '', NULL, '$2a$10$2ZWqz5BdJrqdD6KxxTIdtOW1VP5hR68iVTUQW9bjbiYxiUftf7jXG', NULL, NULL, NULL, NULL),
+(3, 'suraj', 'suraj@gmail.com', '8070865523', 'Super Admin', '1', 'vault', 'gst12345', 'The Box', 'kandivali west', 'maharashtra', 'mumbai', '2023-04-12', '2023-04-15', NULL, '$2y$10$JbTvHVxoK601o7ZmC9Akye658eMiIt1L38PvOHj7wjLKsq4jIOWAG', NULL, '2023-04-12 03:31:21', '2023-04-12 07:36:31', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warehouses`
+--
+
+CREATE TABLE `warehouses` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `total_units` varchar(255) NOT NULL,
+  `area` varchar(255) NOT NULL,
+  `height` varchar(255) NOT NULL,
+  `width` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `warehouses`
+--
+
+INSERT INTO `warehouses` (`id`, `name`, `status`, `total_units`, `area`, `height`, `width`, `location`, `deleted_at`, `created_at`, `updated_at`) VALUES
+(1, 'Raj', '1', '7', '4100', '100', '500', 'mumbai', NULL, '2023-04-12 04:55:46', '2023-04-12 04:55:46'),
+(2, 'Raj', '1', '7', '4100', '100', '500', 'mumbai', NULL, '2023-04-12 04:57:27', '2023-04-12 04:57:27'),
+(3, 'Abhijeet', '1', '8', '4100', '100', '500', 'Mumbai', NULL, '2023-04-12 06:18:53', '2023-04-12 06:18:53');
 
 --
 -- Indexes for dumped tables
@@ -183,11 +251,23 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `units`
+--
+ALTER TABLE `units`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- Indexes for table `warehouses`
+--
+ALTER TABLE `warehouses`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -218,10 +298,22 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `units`
+--
+ALTER TABLE `units`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `warehouses`
+--
+ALTER TABLE `warehouses`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
